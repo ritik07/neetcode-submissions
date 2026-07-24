7423c36d3ed75d483901546bd8be2f61f52e06ea
Ritik Soni <34769340+ritik07@users.noreply.github.com>
2026-07-24 20:46:22 +0530

    Add: buy-and-sell-crypto - submission-3

---
+++ Data Structures & Algorithms/buy-and-sell-crypto/submission-3.js
@@ -0,0 +1,18 @@
+class Solution {
+    /**
+     * @param {number[]} prices
+     * @return {number}
+     */
+    maxProfit(prices) {
+        let min = Infinity
+        let max = 0;
+
+
+        for(let i = 0; i < prices.length; i++){
+            min = Math.min(min, prices[i]);
+            max = Math.max(max, prices[i] - min);
+        }
+        
+        return max
+    }
+}
